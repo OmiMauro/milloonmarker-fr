53fc2bedc6532bada2495bce385bf9d0da073fc3
Manu <ominuka.mauro@gmail.com>
2022-12-16 02:24:59 -0300

    Feat: add Redux to index and persistStore

--- src/index.js
+++ src/index.js
@@ -5,15 +5,24 @@ import 'bootstrap/dist/css/bootstrap.min.css'
 import 'bootstrap/dist/js/bootstrap'
 import './assets/styles.css'
 import { BrowserRouter, Routes, Route } from 'react-router-dom'
-
+import { Provider } from 'react-redux'
+import store from './redux/store'
+// redux
+import { PersistGate } from 'redux-persist/integration/react'
+import { persistStore } from 'redux-persist'
 const root = ReactDOM.createRoot(document.getElementById('root'))
+const persistor = persistStore(store)
 
 root.render(
 	<StrictMode>
-		<BrowserRouter>
-			<Routes>
-				<Route path="/*" element={<App />} />
-			</Routes>
-		</BrowserRouter>
+		<Provider store={store}>
+			<PersistGate loading={null} persistor={persistor}>
+				<BrowserRouter>
+					<Routes>
+						<Route path="/*" element={<App />} />
+					</Routes>
+				</BrowserRouter>
+			</PersistGate>
+		</Provider>
 	</StrictMode>
 )
